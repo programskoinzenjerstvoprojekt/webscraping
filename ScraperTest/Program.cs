a414e8d5bb25530432f29f518b1c591179193ab8
Mattheeeew <44161476+Mattheeeew@users.noreply.github.com>
2019-03-06 21:54:06 +0100

    Scraping Id-a, naslova i eng naslova

---
+++ ScraperTest/Program.cs
@@ -0,0 +1,58 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScraperTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetHtmlAsync();
+            Console.ReadKey();
+        }
+
+        private static async void GetHtmlAsync()
+        {
+            var url = "https://www.kaptolcinema.hr/kupi-ulaznicu-2532";
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+
+            var ProductsHtml = htmlDocument.DocumentNode.Descendants("div")
+                .Where(node => node.GetAttributeValue("id", "")
+                .Equals("all-movies-container")).ToList();
+
+            var ProductListItems = ProductsHtml[0].Descendants("div")
+                .Where(node => node.GetAttributeValue("id", "")
+                .Contains("movie_")).ToList();
+            
+            foreach(var ProductListItem in ProductListItems)
+            {
+                Console.WriteLine(ProductListItem.GetAttributeValue("id", ""));
+                Console.WriteLine(ProductListItem.Descendants("a")
+                    .Where(node => node.GetAttributeValue("class", "")
+                    .Equals("title")).FirstOrDefault().InnerText
+                    );
+                var onclick = ProductListItem.Descendants("a").FirstOrDefault().GetAttributeValue("onclick", "");
+                //Console.WriteLine(ProductListItem.Descendants("a").FirstOrDefault().GetAttributeValue("onclick", ""));
+
+                Console.WriteLine(ProductListItem.Descendants("a")
+                    .Where(node => node.GetAttributeValue("onclick", "")
+                    .Equals(onclick)).LastOrDefault().InnerText
+                    );
+                Console.WriteLine();
+
+
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
